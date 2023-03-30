10f9ef5601baaf3dfee821e482060429e594410b
Dom19740 <dbuzugbe@gmail.com>
2023-03-30 17:45:39 +0100

    upload blackjack and test

--- Blackjack.py
+++ Blackjack.py
@@ -9,11 +9,9 @@ print(logo)
 
 # restart = input("Do you want to play? 'y or 'n': ")
 
-
-
-#cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 #cards = [11, 2, 3, 4, 5, 6, 10]
-cards = [10, 5, 1, 11]
+#cards = [4]
 
 #make dictionaries to hold cards and scores
 player_cards = {
@@ -31,6 +29,12 @@ player_scores = {
 def deal_card(player):
     player_cards[player].append(random.choice(cards))
     player_scores[player] = sum(player_cards[player])
+
+    if player_scores[player] > 21 and 11 in player_cards[player]:
+        player_cards[player].remove(11)
+        player_cards[player].append(1)
+        player_scores[player] = sum(player_cards[player])  
+
     return player_cards, player_scores
 
 
@@ -46,23 +50,15 @@ def show_cards(player):
     if player == "Cpu" and player_scores["Player1"] == 21 and len(
             player_cards["Cpu"]) == 1:
         deal_card("Cpu")
-        
-    if player_scores[player] > 21 and 11 in player_cards[player]:
-        player_cards[player].remove(11)
-        player_cards[player].append(1)
-        player_scores[player] = sum(player_cards[player])       
                 
     cards_str = (" + ".join(str(x) for x in player_cards[player]))
     print(f"\n{player} cards are: {cards_str}")
-    # print(player_cards[player])
     print(f"{player} score is {player_scores[player]}")
     return player_cards, player_scores
 
 #show all
 show_cards("Cpu")
 show_cards("Player1")
-
-# hit_stand = ""
 
 
 #function to assess scores
@@ -73,29 +69,31 @@ def assess_score():
     winner = ""
     hit_stand = ""
 
+    #Player1 Blackjack
     if player_scores["Player1"] == 21 and player_cards["Cpu"] and player_scores["Cpu"] !=21:
         print("\nYou have BLACKJACK. Congratulations, You WIN!!")
-
+    
+    #Both players 21 with 2 cards
     elif player_scores["Player1"] == player_scores["Cpu"] and len(player_cards["Player1"]) == len(player_cards["Cpu"]):
-        # print(f"hit stand = {hit_stand}")
-        # print(player_cards["Player1"]) #DELETE
 
         if len(player_cards["Player1"]) > 2:
             show_cards("Cpu")
         print("\nIt's a draw, no one wins the hand.")
 
+    #Cpu over 21, Bust
     elif player_scores["Cpu"] > 21:
+        # print(player_cards["Cpu"]) #DELETE   
+        # print(player_scores["Cpu"]) #DELETE  
         show_cards("Cpu")
-        if player_scores["Cpu"] > 21:
-            print(player_cards["Cpu"]) #DELETE
-            print("\nCpu is BUST, Congratulations, you WIN!!")
+        print("\nCpu is BUST, Congratulations, you WIN!!")
        
-
+    #Player1 over 21, Bust
     elif player_scores["Player1"] > 21:
         deal_card("Cpu")
         show_cards("Cpu")
         print("\nYou are BUST, you LOSE!!")
 
+    #Both players in game, compare scores and print winner
     elif player_scores["Cpu"] > 16 and player_scores["Cpu"] < 22:
         show_cards("Cpu")
 
@@ -109,28 +107,29 @@ def assess_score():
         else:
             print("\nCpu WINS, UNLUCKY!!")
 
+    #prompt to hit for Player1
     else:
         hit_stand = input("\nType 'h' to Hit or 's' to Stand: ")
         while hit_stand != 'h' and hit_stand != 's':
-            input("\nType 'h' to Hit or 's' to Stand: ")
-
+            hit_stand = input("\nType 'h' to Hit or 's' to Stand: ")
 
     if hit_stand == "h":
         deal_card("Player1")
         show_cards("Player1")
         assess_score()
 
-
     if hit_stand == "s":
         while player_scores["Cpu"] < 17:
             deal_card("Cpu")
-            # print(player_cards["Cpu"])
+            # print(player_cards["Cpu"]) #DELETE  
 
         assess_score()
-        
+
     print("")
-    # return hit_stand 
+
 
 assess_score()
+# print(player_cards["Cpu"]) #DELETE   
+# print(player_scores["Cpu"]) #DELETE   
 
 # play_game()
