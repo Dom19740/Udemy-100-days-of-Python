eff2243f1b185e7247f11c0bcfc3a0805b61db3b
Dom19740 <dbuzugbe@gmail.com>
2023-04-23 15:25:11 +0100

    Implement 1 or 2 player option

--- Day20_SnakeGame/main.py
+++ Day20_SnakeGame/main.py
@@ -15,20 +15,73 @@ screen.bgcolor(SCREEN_COLOR)
 screen.title("THE HUNGRY CATERPILLAR")
 screen.tracer(0)  # turn off screen animation
 
-# Add objects
-color1 = "blue"
-color2 = "yellow"
-snake1 = Snake(color1, [(0, 100), (-20, 100), (-40, 100)])
-snake2 = Snake(color2, [(0, -100), (-20, -100), (-40, -100)])
-food = Food()
-scoreboard1 = Scoreboard((0, 275))
-scoreboard2 = Scoreboard((0, -275))
+game_select = screen.textinput("1 player or 2 player", "Hit 1 or 2 to start").lower()
 
-screen.textinput("Use arrow keys", "Hit any key to start").lower()
+
+def setup_1player_game():
+    # Add objects
+    color1 = "blue"
+    snake1 = Snake(color1, [(0, 100), (-20, 100), (-40, 100)])
+    food = Food()
+    scoreboard1 = Scoreboard((0, 275), color1)
+    return color1, snake1, food, scoreboard1
 
 
 # Game functionality
-def start_game():
+def start_1player_game():
+    # Set up control buttons
+    screen.listen()
+    screen.onkey(snake1.up, "Up")
+    screen.onkey(snake1.down, "Down")
+    screen.onkey(snake1.left, "Left")
+    screen.onkey(snake1.right, "Right")
+
+    game_is_on = True
+    while game_is_on:
+
+        screen.update()  # Update screen each time after all movement is done
+        time.sleep(0.1)
+        snake1.move()
+
+        # Detect collision with food
+        if snake1.head.distance(food) < DIFFICULTY:
+            food.refresh()
+            snake1.extend(color1)
+            scoreboard1.update_score(color1)
+
+        # Detect collision with wall
+        if snake1.head.xcor() > 280 or snake1.head.xcor() < -280 or snake1.head.ycor() > 280 or snake1.head.ycor() < -280:
+            game_is_on = False
+            scoreboard1.game_over()
+
+        # Detect collision with tail
+        for segment in snake1.segments[1:]:
+            if snake1.head.distance(segment) < 10:
+                game_is_on = False
+                scoreboard1.game_over()
+
+    # After the game is over, prompt the user to restart with y/n option
+    restart = screen.textinput("Play again?", "Y or N").lower()
+    if restart == "y":
+        food.refresh()
+        snake1.reset(color1, [(0, 100), (-20, 100), (-40, 100)])
+        scoreboard1.reset((0, 275), color1)
+        start_1player_game()
+
+
+def setup_2player_game():
+    # Add objects
+    color1 = "blue"
+    color2 = "yellow"
+    snake1 = Snake(color1, [(0, 100), (-20, 100), (-40, 100)])
+    snake2 = Snake(color2, [(0, -100), (-20, -100), (-40, -100)])
+    food = Food()
+    scoreboard1 = Scoreboard((0, 275), color1)
+    scoreboard2 = Scoreboard((0, -275), color2)
+    return color1, color2, snake1, snake2, food, scoreboard1, scoreboard2
+
+
+def start_2player_game():
     # Set up control buttons
     screen.listen()
     screen.onkey(snake1.up, "Up")
@@ -52,12 +105,12 @@ def start_game():
         if snake1.head.distance(food) < DIFFICULTY:
             food.refresh()
             snake1.extend(color1)
-            scoreboard1.update_score()
+            scoreboard1.update_score(color1)
 
         if snake2.head.distance(food) < DIFFICULTY:
             food.refresh()
             snake2.extend(color2)
-            scoreboard2.update_score()
+            scoreboard2.update_score(color2)
 
         # Detect collision with wall
         if snake1.head.xcor() > 280 or snake1.head.xcor() < -280 or snake1.head.ycor() > 280 or snake1.head.ycor() < -280:
@@ -85,11 +138,18 @@ def start_game():
         food.refresh()
         snake1.reset(color1, [(0, 100), (-20, 100), (-40, 100)])
         snake2.reset(color2, [(0, -100), (-20, -100), (-40, -100)])
-        scoreboard1.reset((0, 275))
-        scoreboard2.reset((0, -275))
-        start_game()
+        scoreboard1.reset((0, 275), color1)
+        scoreboard2.reset((0, -275), color2)
+        start_2player_game()
 
 
-start_game()
+if game_select == '2':
+    # Call setup_2player_game()
+    color1, color2, snake1, snake2, food, scoreboard1, scoreboard2 = setup_2player_game()
+    start_2player_game()
+else:
+    # Call setup_1player_game()
+    color1, snake1, food, scoreboard1 = setup_1player_game()
+    start_1player_game()
 
 screen.exitonclick()
--- Day20_SnakeGame/scoreboard.py
+++ Day20_SnakeGame/scoreboard.py
@@ -5,38 +5,38 @@ FONT = ('courier', 16, 'normal')
 
 class Scoreboard(Turtle):
 
-    def __init__(self, position):
+    def __init__(self, position, player):
         super().__init__()  # Inherit attributes and methods of Turtle Class, and expand on them
         self.keep_high_score()
-        self.initialise_score(position)
+        self.initialise_score(position, player)
 
     def keep_high_score(self):
         self.highscore = 0
 
-    def initialise_score(self, position):
+    def initialise_score(self, position, player):
         self.color("white")
         self.speed("fastest")
         self.penup()
         self.goto(position)
         self.hideturtle()
         self.score = 0
-        self.write(self.get_score_text(), align="center", font=FONT)
+        self.write(self.get_score_text(player), align="center", font=FONT)
 
-    def update_score(self):
+    def update_score(self, player):
         self.clear()
         self.score += 1
-        self.write(self.get_score_text(), align="center", font=FONT)
+        self.write(self.get_score_text(player), align="center", font=FONT)
 
-    def get_score_text(self):
+    def get_score_text(self, player):
         # Helper method to generate the score text with score and highscore
-        return f"Score: {self.score}. High score: {self.highscore}"
+        return f"{player} score: {self.score}. high score: {self.highscore}"
 
     def game_over(self):
         self.goto(0, 100)
         self.write("GAME OVER", align="center", font=FONT)
 
-    def reset(self, position):
+    def reset(self, position, player):
         if self.score > self.highscore:
             self.highscore = self.score
         self.clear()
-        self.initialise_score(position)
+        self.initialise_score(position, player)
