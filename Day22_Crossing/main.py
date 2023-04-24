c6a583c84107b0f0a73d413549f4c8a8a8711edb
Dom19740 <dbuzugbe@gmail.com>
2023-04-24 15:13:19 +0100

    Finalise code, add levels and lives

--- Day22_Crossing/main.py
+++ Day22_Crossing/main.py
@@ -1,30 +1,69 @@
 import time
+import random
+import turtle
 from turtle import Screen
 from player import Player
 from car_manager import CarManager, Road
 from scoreboard import Scoreboard
+
+TIMER = 0.1
+
 
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("grey")
 screen.tracer(0)
 
-player = Player()
+# Add objects
 road_lower = Road(-265)
 road_upper = Road(270)
+player = Player()
+car_manager = CarManager()
+scoreboard = Scoreboard()
+
+
+
 
 # Set up control button
 screen.listen()
 screen.onkeypress(player.up, "Up")
+screen.onkeypress(player.down, "Down")
 
 game_is_on = True
 while game_is_on:
-    time.sleep(0.1)
+    time.sleep(TIMER)
     screen.update()
 
-    # reset when turtle reaches top
+    # create cars
+    car_manager.create_car()
+
+    # move cars
+    for car in car_manager.all_cars:
+        # Move the car forward by a random distance
+        car.backward(random.randint(1, 10))  # Modify this line to get the car's speed
+
+        # Check if the car has reached the left edge of the screen
+        if car.xcor() < -300:
+            car.hideturtle()  # Optional: Hide the car when it goes off the screen
+            car_manager.all_cars.remove(car)  # Remove the car from the list
+
+    # if a car hits player
+    for car in car_manager.all_cars:
+        if player.distance(car) < 20:
+
+            # if all lives lost, end game
+            if scoreboard.lives < 1:
+                game_is_on = False
+                scoreboard.game_over()
+            else:
+                scoreboard.lose_life()
+                player.reset()
+
+    # reset when player when turtle reaches top
     if player.ycor() > 270:
+        scoreboard.level_up()
         player.reset()
+        TIMER *= 0.9
 
 
 
--- Day22_Crossing/player.py
+++ Day22_Crossing/player.py
@@ -1,12 +1,11 @@
 from turtle import Turtle
 
 STARTING_POSITION = (0, -280)
-MOVE_DISTANCE = 10
+MOVE_DISTANCE = 20
 FINISH_LINE_Y = 280
 
 
 class Player(Turtle):
-
     def __init__(self):
         super().__init__()
         self.shape("turtle")
@@ -17,8 +16,10 @@ class Player(Turtle):
 
     # control movements with arrows
     def up(self):
-        new_y = self.ycor() + 20
-        self.goto(self.xcor(), new_y)
+        self.forward(MOVE_DISTANCE)
+
+    def down(self):
+        self.backward(MOVE_DISTANCE)
 
     # rest when get to top of screen
     def reset(self):
