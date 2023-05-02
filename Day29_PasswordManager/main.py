9dc35854fb758f87fa8ed7b20764a4ed44aa70be
Dom19740 <dbuzugbe@gmail.com>
2023-05-02 13:12:08 +0100

    Error handling in Password Manager

--- Day29_PasswordManager/main.py
+++ Day29_PasswordManager/main.py
@@ -2,6 +2,7 @@ from tkinter import *
 from tkinter import messagebox
 from random import choice, randint, shuffle
 import pyperclip
+import json
 
 FONT = ("Calibri", 10, "normal")
 
@@ -9,7 +10,7 @@ FONT = ("Calibri", 10, "normal")
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #ç
 
 def generate():
-
+    # clear previous entry if there is one
     entry_password.delete(0, END)
 
     # Password Generator Project
@@ -27,35 +28,94 @@ def generate():
 
     password = ''.join(password_list)
 
+    # enter generated password into empty field
     entry_password.insert(0, string=password)
+
+    # copy password to clipboard
     pyperclip.copy(password)
+
+
+# ---------------------------- SEARCH PASSWORD ------------------------------- #
+
+def search():
+
+    #get website to search for
+    search_website = entry_website.get()
+
+    try:
+        # open data file and save as dictionary
+        with open('data.json', 'r') as data_file:
+            # reading data
+            data = json.load(data_file)
+
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message="File not found")
+
+    else:
+        if search_website in data:
+
+            search_email = data[search_website]["email"]
+            search_password = data[search_website]["password"]
+
+            copy_password = messagebox.askokcancel(title=f"Site: {search_website}",
+                                                   message=f"Email:         {search_email}"
+                                                           f"\n"f"Password:  {search_password}"
+                                                           f"\n\nCopy password to clipboard")
+            if copy_password:
+                # copy password to clipboard
+                pyperclip.copy(search_password)
+
+        else:
+            messagebox.askokcancel(title="Error", message="No Site found")
 
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 def add():
-    # get the data from the entry widgets
+    is_ok = False
+
+    # get the data from the entry fields
     website = entry_website.get()
     email = entry_email.get()
     password = entry_password.get()
-    is_ok = False
+    new_data = {
+        website: {
+            "email": email,
+            "password": password,
+        }
+    }
 
     if len(website) == 0 or len(email) == 0 or len(password) == 0:
         messagebox.showinfo(title="Error", message="Fill in all fields")
-    else:
-        is_ok = messagebox.askokcancel(title="Save", message=f"Website:    {website}\n"
-                                                             f"Email:         {email}\n"
-                                                             f"Password:  {password}\n\n"
-                                                             f"Continue to save?")
-    if is_ok:
-        # open the file for appending
-        with open('data.txt', 'a') as data_file:
-            # write the data to the file as a line
-            data_file.write(f'{website} | {email} | {password}\n')
 
-        # clear the entry widgets for the next set of data
-        entry_website.delete(0, END)
-        entry_password.delete(0, END)
+    else:
+        is_ok = messagebox.askokcancel(title="Save", message=f"Site:              {website}\n"
+                                                             f"Email:            {email}\n"
+                                                             f"Password:    {password}\n\n"
+                                                             f"Save and copy password to Clipboard?")
+    if is_ok:
+        # open the file in read mode
+        try:
+            with open('data.json', 'r') as data_file:
+                # reading old data
+                data = json.load(data_file)
+
+        except FileNotFoundError:
+            with open('data.json', 'w') as data_file:
+                # saving new data
+                json.dump(new_data, data_file, indent=4)
+
+        else:
+            # updating old data with new data
+            data.update(new_data)
+            with open('data.json', 'w') as data_file:
+                # saving updated data
+                json.dump(data, data_file, indent=4)
+
+        finally:
+            # clear the entry widgets for the next set of data
+            entry_website.delete(0, END)
+            entry_password.delete(0, END)
 
 
 # ---------------------------- UI SETUP ------------------------------- #
@@ -70,13 +130,17 @@ canvas.create_image(100, 100, image=logo_img)
 canvas.grid(row=0, column=1)
 
 # row 1
-label_website = Label(text="Website:", font=FONT)
+label_website = Label(text="Site:", font=FONT)
 label_website.grid(row=1, column=0)
 
 entry_website = Entry(width=35)
+entry_website.insert(0, string="amazon")
 entry_website.get()
 entry_website.focus()
-entry_website.grid(row=1, column=1, columnspan=2)
+entry_website.grid(row=1, column=1)
+
+button_search = Button(text="Search", command=search, font=FONT)
+button_search.grid(row=1, column=2)
 
 # row 2
 label_email = Label(text="Email/User:", font=FONT)
@@ -85,7 +149,7 @@ label_email.grid(row=2, column=0)
 entry_email = Entry(width=35)
 entry_email.insert(0, string="bob@gmail.com")
 entry_email.get()
-entry_email.grid(row=2, column=1, columnspan=2)
+entry_email.grid(row=2, column=1)
 
 # row 3
 label_password = Label(text="Password", font=FONT)
@@ -93,13 +157,13 @@ label_password.grid(row=3, column=0)
 
 entry_password = Entry(width=35)
 entry_password.get()
-entry_password.grid(row=3, column=1, columnspan=2)
+entry_password.grid(row=3, column=1)
 
 button_generate = Button(text="Generate", command=generate, font=FONT)
-button_generate.grid(row=3, column=2)
+button_generate.grid(row=2, column=2, rowspan=2)
 
 # row 4
 button_add = Button(text="Add", command=add, width=10, font=FONT)
-button_add.grid(row=4, column=1, columnspan=2)
+button_add.grid(row=4, column=1)
 
 window.mainloop()
