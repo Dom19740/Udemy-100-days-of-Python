14cddb18fc06eeeb4778fd0b7a6ac579146c3a80
Dom19740 <dbuzugbe@gmail.com>
2023-05-02 13:11:51 +0100

    class day 30

--- Day1-16/Day7_Hangman.py
+++ Day1-16/Day7_Hangman.py
@@ -1,77 +1,76 @@
 import random
-import hangman_words
 import os
-
+from data.hangman_words import word_list
+from art.hangman_art import logo, stages
 
 os.system('cls')
-print(hangman_art.logo)
+print(logo)
 
-chosen_word = random.choice(hangman_words.word_list)
+chosen_word = random.choice(word_list)
 
-#Testing code
-#print(f'Pssst, the solution is {chosen_word}.\n')
+# Testing code
+# print(f'Pssst, the solution is {chosen_word}.\n')
 
-#Create blanks
+# Create blanks
 display = []
 for letter in chosen_word:
-  display += "_"
+    display += "_"
 print(f"\n{' '.join(display)}")
 
-
-#Check guessed letter while there are still blanks and 10a lives
+# Check guessed letter while there are still blanks and 10a lives
 lives = 9
 guess_attempts = []
 
 while "_" in display:
-  
-  guess = input("\nGuess a letter: ").lower()
-  os.system('cls')
 
-  i = 0
-  correct_guess = False
+    guess = input("\nGuess a letter: ").lower()
+    os.system('cls')
 
-  if guess in guess_attempts:
-      print(hangman_art.logo)
-      print(f"\n{' '.join(display)}")
-      print(f"\n{' '.join(guess_attempts)}")
-      print(hangman_art.stages[lives])
-      print(f"\nYou already tried that letter, try again.")
+    i = 0
+    correct_guess = False
 
-    
-  else:
-      # check to see if guess is in chosen word
-      for letter in chosen_word:
-          if letter == guess:
-              display[i] = guess
-              correct_guess = True
-          i += 1
+    if guess in guess_attempts:
+        print(logo)
+        print(f"\n{' '.join(display)}")
+        print(f"\n{' '.join(guess_attempts)}")
+        print(stages[lives])
+        print(f"\nYou already tried that letter, try again.")
 
-      #add guess attempts to list
-      guess_attempts.append(guess)
-  
-      #if guess is correct
-      if (correct_guess == True):
-          print(hangman_art.logo)
-          print(f"\n{' '.join(display)}")
-          print(f"\n{' '.join(guess_attempts)}")
-          print(hangman_art.stages[lives])
-          print("\nNice!")
-        
-      #if guess is wrong
-      else:
-          lives -= 1
-          print(hangman_art.logo)
-          print(f"\n{' '.join(display)}")
-          print(f"\n{' '.join(guess_attempts)}")
-          print(hangman_art.stages[lives])
-          print(f"\n'{guess}' is not in the word, you lose a life!")
-      
-      #if lost
-      if lives == 0:
-          print(f"\nDEAD! You Lose. The word was {chosen_word}.\n")
-          break
-    
-      #if won
-      if "_" not in display:
-          print("\nGOOD JOB, You Win!!\n")
-          break
+
+    else:
+        # check to see if guess is in chosen word
+        for letter in chosen_word:
+            if letter == guess:
+                display[i] = guess
+                correct_guess = True
+            i += 1
+
+        # add guess attempts to list
+        guess_attempts.append(guess)
+
+        # if guess is correct
+        if correct_guess:
+            print(logo)
+            print(f"\n{' '.join(display)}")
+            print(f"\n{' '.join(guess_attempts)}")
+            print(stages[lives])
+            print("\nNice!")
+
+        # if guess is wrong
+        else:
+            lives -= 1
+            print(logo)
+            print(f"\n{' '.join(display)}")
+            print(f"\n{' '.join(guess_attempts)}")
+            print(stages[lives])
+            print(f"\n'{guess}' is not in the word, you lose a life!")
+
+        # if lost
+        if lives == 0:
+            print(f"\nDEAD! You Lose. The word was {chosen_word}.\n")
+            break
+
+        # if won
+        if "_" not in display:
+            print("\nGOOD JOB, You Win!!\n")
+            break
---
+++ class_day30_Errors/main.py
@@ -0,0 +1,27 @@
+# Keyword Method with iterrows()
+# {new_key:new_value for (index, row) in df.iterrows()}
+
+import pandas
+
+data = pandas.read_csv("nato_phonetic_alphabet.csv")
+
+# Create a dictionary in this format:
+phonetic_dict = {row.letter: row.code for (index, row) in data.iterrows()}
+print(phonetic_dict)
+
+# Create a list of the phonetic code words from a word that the user inputs.
+
+
+def generate_word():
+    word = input("Enter a word: ").upper()
+
+    try:
+        output_list = [phonetic_dict[letter] for letter in word]
+    except KeyError:
+        print("Sorry, only letters")
+        generate_word()
+    else:
+        print(output_list)
+
+
+generate_word()
